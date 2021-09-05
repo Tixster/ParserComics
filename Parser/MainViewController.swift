22e3086e9f78e1c68ba66c19c9c35c022f349c9e
Kirill Tila <gauter51@gmail.com>
2021-09-05 22:39:12 +0700

    Подготовил проект: удалил сториборд, файл Scene, добавил SnapKit и SwiftSoup

---
+++ Parser/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  Parser
+//
+//  Created by Кирилл Тила on 05.09.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
+        view.backgroundColor = .white
+    }
+
+
+}
+
